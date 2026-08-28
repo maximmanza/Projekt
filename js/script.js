2f065f0cbbd49dbc592493179a83f312f65acaa2
maximmanza <147446134+maximmanza@users.noreply.github.com>
2026-08-28 11:16:18 +0200

    ferdig

--- js/script.js
+++ js/script.js
@@ -29,15 +29,60 @@ function nullstill() {
     document.getElementById("poeng2").innerText = poeng2;
 }
 
+
 function farge() {
     document.body.style.backgroundColor =
-        document.body.style.backgroundColor === "black" ? "white" : "black";
+        document.body.style.backgroundColor === "black"
+        ? "white"
+        : "black";
 
     document.body.style.color =
-        document.body.style.color === "white" ? "black" : "white";
+        document.body.style.color === "white"
+        ? "black"
+        : "white";
 
     document.getElementById("buton").src =
         document.getElementById("buton").src.includes("sol")
         ? "/bilde/mørk.png"
         : "/bilde/sol.png";
+}
+
+
+
+let sitater = [
+    "Penger gjør deg ikke lykkelig, men det er mer behagelig å gråte i en Ferrari",
+    "Jeg er så smart at jeg noen ganger ikke forstår et eneste ord av det jeg sier",
+    "Livet er for kort til å være alvorlig hele tiden. Så om du ikke kan le av deg selv, ring meg, så skal jeg le av deg",
+    "Husk alltid at du er helt unik. Akkurat som alle andre",
+    "Alder har ingen betydning, med mindre du er en ost"
+];
+
+function nyttSitat() {
+    let tilfeldig = Math.floor(Math.random() * sitater.length);
+
+    document.getElementById("sitat").innerText = sitater[tilfeldig];
+}
+
+
+let bilder = [
+    "/bilde/balet.jpg",
+    "/bilde/biznes.jpg",
+    "/bilde/cr7.jpg",
+    "/bilde/krutoi.jpg",
+    "/bilde/pablo.jpg",
+    "/bilde/pivo.jpg",
+    "/bilde/saske.jpg",
+    "/bilde/sigma.jpg",
+    "/bilde/vaca.jpg",
+    "/bilde/what.jpg",
+    "/bilde/zakuril.jpg",
+    "/bilde/fast.png",
+
+
+];
+
+function nyttBilde() {
+    let tilfeldig = Math.floor(Math.random() * bilder.length);
+
+    document.getElementById("bilde").src = bilder[tilfeldig];
 }
